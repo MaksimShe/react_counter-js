aad750e6a8d2d667ddbb374f94264dd33b231b5d
SheFing <shevchukm2002@gmail.com>
2025-10-03 22:01:16 +0300

    fck u ai buddy 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ export const App = () => {
   // fuck u ai buddy u are fucking idiot
 
   const addOne = () => {
-    setCount(number => number + 1);
+    setCount(prev => prev + 1);
   };
 
   const add100 = () => {
-    setCount(number => number + 100);
+    setCount(prev => prev + 100);
   };
 
   // DON'T change the code below
